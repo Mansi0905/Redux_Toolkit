0331c76a1dc064248964230d78fab081ae2d8b6f
Mansi0905 <jaipur6696@gmail.com>
2026-09-07 21:28:53 +0530

    learning the basics of react-toolkit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div>
+      <h1>0</h1>
+      <button>Increment</button>
+      <button>Decremenet</button>
+    </div>
+  )
+}
+
+export default App
