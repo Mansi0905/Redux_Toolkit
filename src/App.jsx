4789890a6f9bc3a7834b4c6f159a2d9c0c1428b1
Mansi0905 <jaipur6696@gmail.com>
2026-09-08 12:01:39 +0530

    adding the useDispatch and useSelector

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,26 @@
 import React from 'react'
+import { useDispatch , useSelector} from 'react-redux'
 
 const App = () => {
+
+const dispatch = useDispatch()
+const count = useSelector()
+
   return (
     <div>
       <h1>0</h1>
-      <button>Increment</button>
-      <button>Decremenet</button>
+
+      <button onClick={()=>{
+         
+      }}>
+        Increment
+        </button>
+
+      <button onClick={()=>{
+         
+      }}>
+        Decrement
+      </button>
     </div>
   )
 }
