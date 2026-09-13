519e29e74bc41c9d75bed3e4258ef2a068655f71
Mansi0905 <jaipur6696@gmail.com>
2026-09-13 21:10:52 +0530

    dependencies

--- Redux-project/src/App.jsx
+++ Redux-project/src/App.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const App = () => {
   return (
-    <div className='h-screen w-full bg-gray-800'>
+    <div className='h-screen w-full text-white bg-gray-900'>
       <h1></h1>
       
     </div>
