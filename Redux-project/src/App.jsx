be97143107dfcab623e4592dba05bbaa50d5cee6
Mansi0905 <jaipur6696@gmail.com>
2026-09-23 21:18:34 +0530

    adding async await

--- Redux-project/src/App.jsx
+++ Redux-project/src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { fetchPhotos } from './api/mediaApi'
+import { fetchPhotos, fetchVideos } from './api/mediaApi'
 
 
 const App = () => {
@@ -7,10 +7,20 @@ const App = () => {
   
   
   return (
-    <div className='h-screen w-full text-white bg-gray-900'>
-      <button onClick={()=>{
-        fetchPhotos('cat')
+    <div className='h-screen w-full flex flex-col items-start  gap-4 text-white bg-gray-900'>
+      <button className='bg-green-400 px-4 py-2 m-5 ' onClick={async()=>{
+       const data= await fetchPhotos('cat')
+
+       console.log(data.results);
+       
       }}>GET PHOTOS</button>
+
+      <button className='bg-green-400  px-4 py-2 m-5 ' onClick={async()=>{
+       const data= await fetchVideos('cat')
+
+       console.log(data.videos);
+       
+      }}>GET VIDEOS</button>
       
     </div>
   )
--- Redux-project/src/api/mediaApi.js
+++ Redux-project/src/api/mediaApi.js
@@ -5,13 +5,22 @@ const PEXELS_KEY = import.meta.env.VITE_PEXELS_KEY
 
 
 
-export function fetchPhotos(query){
-   const res= axios.get('https://api.unsplash.com/search/photos',{
-    params:{query},
-    headers:{}
+export async function fetchPhotos(query,page =1, per_page=20){
+   const res= await axios.get('https://api.unsplash.com/search/photos',{
+    params:{query,page,per_page },
+    headers:{Authorization:`Client-ID ${UNSPLASH_KEY}`}
    })
 
-   console.log(res);
+   return res.data;
    
+}
+
+export async function fetchVideos(query,per_page=15){
+
+   const res= await axios.get('https://api.pexels.com/videos/search',{
+    params:{query,per_page },
+    headers:{Authorization:PEXELS_KEY}
+   })
+   return res.data
 }
 
